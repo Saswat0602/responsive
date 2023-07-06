bef2008afbbd282fa2d4ae3a2684bbfcce84f8a7
saswat <saswatranjan0602@gmail.com>
2023-07-06 20:19:38 +0530

    nav-bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">hello </h1>
+    <div>
+
+    <Navbar/>
+  
     </div>
   );
 }
