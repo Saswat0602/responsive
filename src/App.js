e268712e766cac986c3b4926896313be7513b7e7
saswat <saswatranjan0602@gmail.com>
2023-07-06 20:44:15 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
+import Analytics from './components/Analytics';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-
-    <Navbar/>
-    <Hero/>
-  
+      <Navbar />
+      <Hero />
+      <Analytics />
     </div>
   );
 }
